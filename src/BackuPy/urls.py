967a68131c85fc6896efce87b1b0b74497c54cf0
Pablo N. Dalmasso <pablodalmasso@gmail.com>
2015-02-23 01:31:29 -0300

    "Se acomoda interfaz grafica"

--- src/BackuPy/urls.py
+++ src/BackuPy/urls.py
@@ -7,6 +7,21 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'django.contrib.auth.views.login'),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^main/$', 'apps.backup.views.main'),
+    url(
+        r'^list_backup/$',
+        'apps.backup.views.list_backup'
+    ),
+    url(
+        r'^detail_backup/(?P<id_backup>\d+)/$',
+        'apps.backup.views.list_files'
+    ),
+    url(
+        r'^download/(?P<id_backup>\d+)/(?P<name_file>.*)/$',
+        'apps.backup.views.download_backup'
+    ),
 )
--- src/apps/backup/views.py
+++ src/apps/backup/views.py
@@ -1,3 +1,80 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+import os
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from apps.backup.models import Backup
 
-# Create your views here.
+
+@login_required
+def main(request):
+    return render_to_response(
+        'main.html',
+        RequestContext(
+            request,
+            {}
+        )
+    )
+
+
+@login_required
+def list_backup(request):
+    backup = Backup.objects.all()
+    return render_to_response(
+        'backup_list.html',
+        RequestContext(
+            request,
+            {
+                'backup': backup
+            }
+        )
+    )
+
+
+@login_required
+def list_files(request, id_backup):
+    import time
+    file_list = []
+    backup = Backup.objects.get(pk=id_backup)
+    for filename in os.listdir(backup.destination):
+        print filename
+        info = os.stat(backup.destination+'/'+filename)
+        unidad = 'b.'
+        size = info.st_size
+        if size > 1024:
+            unidad = 'Kb.'
+            size = size / 1024
+        if size > 1024:
+            unidad = 'Mb.'
+            size = size / 1024
+        if size > 1024:
+            unidad = 'Gb.'
+            size = size / 1024
+        s = {
+            'name': filename,
+            'size': str(size) + ' '+ unidad,
+            'date_create': time.asctime(
+                time.localtime(info.st_ctime)
+            ),
+        }
+        file_list.append(s)
+    return render_to_response(
+        'file_list.html',
+        RequestContext(
+            request,
+            {
+                'file_list': file_list,
+                'backup': backup
+            }
+        )
+    )
+
+
+@login_required
+def download_backup(request, id_backup, name_file):
+    from django.views.static import serve
+    backup = Backup.objects.get(pk=id_backup)
+    filepath = backup.destination + '/' + name_file
+    return serve(
+        request, os.path.basename(filepath), os.path.dirname(filepath)
+    )
